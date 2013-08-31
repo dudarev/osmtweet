9bdae38c69d634d03f9087d17349ffade5416be8
Artem Dudarev <dudarev@gmail.com>
2013-08-31 23:49:55 +0300

    fixed urls trimming problems

--- src/trim_to_tweet.py
+++ src/trim_to_tweet.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 import unittest
 
+MAX_URL_LENGTH = 50
+MAX_TWEET_LENGTH = 140
+
 def remove_word(s):
-    words = s.split(' ')
-    i = len(words)-1
+    words = s.strip().split(' ')
+    i_max = len(words)-1
+    i = i_max
     has_dots = False
-    while i>0:
+    while i>=0:
         if not words[i] == '...':
-            if not 'http:' in words[i]:
+            if (not 'http:' in words[i]) or (len(words[i]) > MAX_URL_LENGTH) or (i<i_max):
                 if has_dots:
                     del words[i]
                 else:
@@ -19,9 +23,9 @@ def remove_word(s):
     return ' '.join(words)
 
 def trim_to_tweet(s):
-    if len(s) <= 140:
+    if len(s) <= MAX_TWEET_LENGTH:
         return s
-    while len(s) > 140:
+    while len(s) > MAX_TWEET_LENGTH:
         s = remove_word(s)
     return s
 
@@ -39,7 +43,7 @@ class TestSequenceFunctions(unittest.TestCase):
         self.assertEqual(remove_word(old),new)
 
     def testTrimToTweet(self):
-        old = 'less then 140'
+        old = 'less then MAX_TWEET_LENGTH'
         new = old
         self.assertEqual(trim_to_tweet(old), new)
         old = 'Tiger Woods Voicemail Slow Jam Remix Tiger Woods Voicemail Slow Jam Remix Tiger Woods Voicemail Slow Jam Remix Tiger Woods Voicemail Slow Jam Remix http://toptuby.appspot.com/?v=OEkomaBTppY'
@@ -52,8 +56,19 @@ class TestSequenceFunctions(unittest.TestCase):
         http://www.openstreetmap.org/browse/changeset/15210559
         """
         new = trim_to_tweet(old)
+        self.assertTrue(len(new) <= MAX_TWEET_LENGTH)
+
+    def testTrimVeryLongURL(self):
+        old = "http://example.com/A_VERY_LONG_URL" + 200*'a'
+        new = trim_to_tweet(old)
+        self.assertTrue(len(new) <= MAX_TWEET_LENGTH)
+
+    def testSeveralShortURLs(self):
+        old = 20 * "http://example.com/A_SHORT_URL "
+        new = trim_to_tweet(old)
         print new
-        self.assertTrue(len(new) <= 140)
+        self.assertTrue(len(new) <= MAX_TWEET_LENGTH)
+
 
 if __name__ == '__main__':
     unittest.main()
